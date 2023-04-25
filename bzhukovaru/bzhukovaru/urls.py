af5ce38df47e62004f83a7012b580a32db77984b
Azamat <BzhukovAA@gmail.com>
2023-04-25 19:11:25 +0300

    telegram widget

--- bzhukovaru/bzhukovaru/urls.py
+++ bzhukovaru/bzhukovaru/urls.py
@@ -1,23 +1,14 @@
-"""
-URL configuration for bzhukovaru project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
+from social_django.views import auth, complete
 
 urlpatterns = [
-    path('', include('telegram_login.urls')),
+    path('', include('telegram_login.urls', namespace='social')),
     path('admin/', admin.site.urls),
+    path('auth/', auth, name='social_auth'),
+    path('auth/complete/', complete, name='social_complete'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
 ]
--- bzhukovaru/telegram_login/views.py
+++ bzhukovaru/telegram_login/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'main.html')
+
+
+
