079a441d3bf52bc0326c5f948e8a912aaa2b0006
Azamat <BzhukovAA@gmail.com>
2023-04-26 13:59:40 +0300

    telegram widget

--- bzhukovaru/telegram_login/views.py
+++ bzhukovaru/telegram_login/views.py
@@ -11,7 +11,7 @@ bot_token = settings.TELEGRAM_BOT_TOKEN
 redirect_url = settings.TELEGRAM_LOGIN_REDIRECT_URL
 
 def main(request):
-    data = ''
+    data = '111'
     if request.GET:
         data=request.GET.get('hash')
     context = {
