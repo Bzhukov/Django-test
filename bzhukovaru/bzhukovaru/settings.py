e0bddcd72181db0614f3240624b5bdd9fa630aad
Azamat <BzhukovAA@gmail.com>
2023-04-27 10:43:00 +0300

    telegram widget

--- bzhukovaru/bzhukovaru/settings.py
+++ bzhukovaru/bzhukovaru/settings.py
@@ -16,11 +16,12 @@ SECRET_KEY = 'django-insecure-m$6dl&*qb82x000z2umb^$5w9s^4ug22_2+%_rv&!q!9t#ak$v
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -31,6 +32,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
