fc6818078d8d38ced451bd0be96cd7bf30ebc9fc
Azamat <BzhukovAA@gmail.com>
2023-05-03 22:43:40 +0300

    telegram widget

--- bzhukovaru/telega/urls.py
+++ bzhukovaru/telega/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import index
+from .views import index,auth
 
 app_name = 'telega'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('222/', auth, name='index'),
     ]
--- bzhukovaru/telega/views.py
+++ bzhukovaru/telega/views.py
@@ -1,8 +1,17 @@
+import asyncio
+import os
 
-
+import telegram.ext
 from django.shortcuts import render
-
-
+from dotenv import load_dotenv
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
+def auth(request):
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    asyncio.run(bot.send_message(124987663, request.GET))
+    return render(request, 'index.html')
